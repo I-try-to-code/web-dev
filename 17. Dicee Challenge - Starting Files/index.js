a6ba18d3eb0184b7a9001c0e6c6e3d578b32ad66
Ayan Gattani <ayangattani@gmail.com>
2024-04-09 17:23:17 +0530

    changing image 1 according to random number

--- 17. Dicee Challenge - Starting Files/index.js
+++ 17. Dicee Challenge - Starting Files/index.js
@@ -2,3 +2,22 @@ var random_number ;
 random_number= Math.random();
 random_number=random_number*7;
 random_number= Math.floor(random_number);
+
+if (random_number==1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+}
+else if (random_number==2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
+}
+else if (random_number==3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
+}
+else if (random_number==4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
+}
+else if (random_number==5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+}
+else if (random_number==6){
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+}
