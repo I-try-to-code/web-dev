48b0372544b89d31c22141ea61a39f37bd671fb1
Ayan Gattani <ayangattani@gmail.com>
2024-04-11 23:48:22 +0530

    w

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -1,6 +1,6 @@
 for (var i=0,i<7,i++){
-    document.querySelector(".drum")[i].addEventListener(click,function (){
-        var audio= new Audio('snare.mp3');
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        var audio= new Audio('sounds/snare.mp3');
         audio.play();
-    })
+    });
 }
