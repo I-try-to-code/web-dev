cbefe4833b7deaaf1f5b58a083cf2b2151ed31c6
Ayan Gattani <ayangattani@gmail.com>
2024-04-11 23:50:20 +0530

    , -> ; in for loop

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -1,4 +1,4 @@
-for (var i=0,i<7,i++){
+for (var i=0;i<7;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
         var audio= new Audio('sounds/snare.mp3');
         audio.play();
