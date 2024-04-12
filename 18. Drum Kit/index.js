7acf913fa3651fbb3b018e6d545e24100e6879e8
Ayan Gattani <ayangattani@gmail.com>
2024-04-13 00:04:41 +0530

    trying to add audio-3

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -1,7 +1,8 @@
-var no_of_drums=document.querySelectorAll(".drum").length;
-for (var i=0;i<no_of_drums;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-        var audioo = new Audio("sounds/tom-3.mp3");
-        audioo.play();
+var no_of_drums = document.querySelectorAll(".drum").length;
+for (var i = 0; i < no_of_drums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audioFile = "sounds/tom-1.mp3"; // Dynamically select the audio file based on the index
+        var audio = new Audio(audioFile);
+        audio.play();
     });
-}   
+}
