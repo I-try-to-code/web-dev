253e37e6a3318cb84f6196b3623a5c20937ed606
Ayan Gattani <ayangattani@gmail.com>
2024-04-12 23:57:43 +0530

    changing sound

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -1,7 +1,7 @@
 var no_of_drums=document.querySelectorAll(".drum").length;
 for (var i=0;i<no_of_drums;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-        var audioo = new Audio("sounds/crash.mp3");
+        var audioo = new Audio("sounds/tom-3.mp3");
         audioo.play();
     });
 }   
