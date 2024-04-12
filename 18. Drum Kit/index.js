1f763bac9a9b18c31009a9dda6783ba35125a022
Ayan Gattani <ayangattani@gmail.com>
2024-04-12 23:54:14 +0530

    trying to add sound 1

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -1,6 +1,6 @@
 for (var i=0;i<7;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-        var audioo = new Audio("sounds/snare.mp3");
+        var audioo = new Audio("sounds/crash.mp3");
         audioo.play();
     });
-} 
+}   
