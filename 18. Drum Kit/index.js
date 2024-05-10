5985ccb4198702e5d61407f897f53313e968e429
Ayan Gattani <ayangattani@gmail.com>
2024-05-10 17:31:57 +0530

    adding keyboard event listeners to drum kit website

--- 18. Drum Kit/index.js
+++ 18. Drum Kit/index.js
@@ -21,7 +21,42 @@ for (var i = 0; i < no_of_drums; i++) {
                 var audioFile = "sounds/crash.mp3";
                 break;
             case "k":
-                var audioFile = "sounds/kick.mp3";
+                var audioFile = "sounds/kick-bass.mp3";
+                break;
+            case "l":
+                var audioFile = "sounds/snare.mp3";
+                break;
+            default:
+                // Default case if none of the above conditions are met
+                return;
+        }
+
+        // Create the audio object and play the corresponding sound
+        var audio = new Audio(audioFile);
+        audio.play();
+    });
+}
+for (var i = 0; i < no_of_drums; i++) {
+    document.addEventListener("keydown", function (event) {
+
+        switch (event.key) {
+            case "w":
+                var audioFile = "sounds/tom-1.mp3";
+                break;
+            case "a":
+                var audioFile = "sounds/tom-2.mp3";
+                break;
+            case "s":
+                var audioFile = "sounds/tom-3.mp3";
+                break;
+            case "d":
+                var audioFile = "sounds/tom-4.mp3";
+                break;
+            case "j":
+                var audioFile = "sounds/crash.mp3";
+                break;
+            case "k":
+                var audioFile = "sounds/kick-bass.mp3";
                 break;
             case "l":
                 var audioFile = "sounds/snare.mp3";
