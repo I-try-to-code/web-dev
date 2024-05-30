b909f8f7fa135dd74c9aea6305464524f22a3459
Ayan Gattani <ayangattani@gmail.com>
2024-05-30 10:43:12 +0530

    fiddling with jquery

--- 19. JQuery/index.js
+++ 19. JQuery/index.js
@@ -5,3 +5,9 @@ $("h1").css("color","blue");
 $("h1");
 //selecting multiple elements
 $("button");
+//use normal notation for selecting elements , can be combined ex: #ID.classname h1
+//for manipulating css styles, use the .css method
+$("h1").css("background-color","yellow");
+$("h1").addClass("big-titl marr");
+$("h1").text("ddfjd");
+$("button").html("hello<hr>")
