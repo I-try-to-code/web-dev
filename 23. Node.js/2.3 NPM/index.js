803929c1ef86d614e7031f96eb94754ec5da26b9
Ayan Gattani <ayangattani@gmail.com>
2024-06-25 22:39:16 +0530

    using npm libraries

--- 23. Node.js/2.3 NPM/index.js
+++ 23. Node.js/2.3 NPM/index.js
@@ -1,3 +1,9 @@
-var abc = require("sillyname");
-var sillyname = abc();
-console.log('my name is '+ sillyname + '. ' );
+//var abc = require("sillyname");
+//import abc from "sillyname";
+//var sillyname = abc();
+//console.log('my name is '+ sillyname + '. ' );
+
+import {randomSuperhero} from 'superheroes';
+
+var name= randomSuperhero();
+console.log('my favourite superhero (other than my parents)  is:  '+ name +"!!");
