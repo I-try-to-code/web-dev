c4530092070c634e41a7e8135acd0d773b26aee0
Ayan Gattani <ayangattani@gmail.com>
2024-06-30 21:35:06 +0530

    comment

--- 23. Node.js/2.4 QR Code Project/index.js
+++ 23. Node.js/2.4 QR Code Project/index.js
@@ -9,3 +9,5 @@ var qr_svg = qr.image('I love QR!', { type: 'svg' });
 qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
  
 var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+
+//this wikl give you a webgage leading qr code
